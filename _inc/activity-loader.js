8df14af28b1a185209eb64525ca48ffcd619eaa6
Brajesh Singh <brajesh@cosmiccoders.com>
2019-01-11 02:00:27 +0530

    Use a better strategy for loading activity, do not lead manually, let themes/BuddyPress handle it for us

--- _inc/activity-loader.js
+++ _inc/activity-loader.js
@@ -1,31 +1,31 @@
 /**
- * Please note, this plugin requires the jQuery cookie plugin, which comes bundled with the bp-default theme and many BuddyPress themes.
- * If you theme does not include it it, please copy it from bp-default/_inc/global.js and paste in your js file
- **/
-jQuery( document ).ready( function() {
-    var jq = jQuery;
-    var is_activity_loading = false;//We'll use this variable to make sure we don't send the request again and again.
+ * With version 2.0+, we simply simulate the click and the themes do the loading for us :)
+ */
+jQuery( document ).ready( function ($) {
+	// We'll use this variable to make sure we don't send the request again and again.
+	var $window = $( window );
 
-    //Check the window scroll event.
-    jq( window ).scroll( function() {
-       //Find the visible "load more" button.
-       //since BP does not remove the "load more" button, we need to find the last one that is visible.
-        var load_more_btn = jq( '.load-more:visible' );
-        //If there is no visible "load more" button, we've reached the last page of the activity stream.
-        if( ! load_more_btn.get(0))
-            return;
-        
-        //Find the offset of the button.
-         var pos = load_more_btn.offset();
-       
-       //If the window height+scrollTop is greater than the top offset of the "load more" button, we have scrolled to the button's position. Let us load more activity.
-          
-       if( jq( window ).scrollTop() + jq( window ).height() > pos.top ) {
-           
-            load_more_btn.trigger('click');
-       }
-    
-    });
+	// Check the window scroll event.
+	$window.scroll( function () {
+		// Find the visible "load more" button.
+		// since BP does not remove the "load more" button, we need to find the last one that is visible.
+		var $load_more_btn = $( '.load-more:visible' );
+		// If there is no visible "load more" button, we've reached the last page of the activity stream.
+		if ( ! $load_more_btn.get( 0 ) ) {
+			return;
+		}
 
-} );//end of dom ready
+		// Find the offset of the button.
+		var pos = $load_more_btn.offset();
+		var offset = pos.top - 50;// 50 px before we reach the button.
+
+		// If the window height+scrollTop is greater than the top offset of the "load more" button,
+		// we have scrolled to the button's position. Let us load more activity.
+		if ($window.scrollTop() + $window.height() > offset) {
+			$load_more_btn.trigger( 'click' );
+		}
+
+	});
+
+});// end of dom ready.
 
--- bp-activity-autoloader.php
+++ bp-activity-autoloader.php
@@ -4,20 +4,23 @@
  * Plugin URI: http://buddydev.com/plugins/bp-activity-autoloader/
  * Author: Brajesh Singh(BuddyDev)
  * Author URI: http://buddydev.com/members/sbrajesh/
- * Version: 1.0.2
+ * Version: 2.0
  * Description: Autoload next page of the BuddyPress Activity Stream when the end of the current page is reached
  */
 
-//just trying to recreate the magic, should we?
+// just trying to recreate the magic, should we?
 
-
+/**
+ * Load autoloader js
+ */
 function bp_activity_autoloader_inc_js() {
-	
-	//if it is a mobile browser and autoloader is disabled, do not load js
+
+	// if it is a mobile browser and autoloader is disabled, do not load js.
 	if ( wp_is_mobile() && apply_filters( 'bp_activity_autoloader_disabled', false ) ) {
-		return ;
+		return;
 	}
-	
-    wp_enqueue_script( 'activity-auto-loader',  plugin_dir_url( __FILE__ ) . '_inc/activity-loader.js', array( 'jquery' ) );//should we make it dependent on 'dtheme-ajax-js'
+
+	wp_enqueue_script( 'activity-auto-loader', plugin_dir_url( __FILE__ ) . '_inc/activity-loader.js', array( 'jquery' ) );
 }
-add_action( 'bp_enqueue_scripts','bp_activity_autoloader_inc_js', 30 );
+
+add_action( 'bp_enqueue_scripts', 'bp_activity_autoloader_inc_js', 30 );
